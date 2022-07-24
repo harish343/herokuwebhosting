36d65b8d0f1c0996840b8191796e08e5bb2ec9e3
harish <harishnarnaware75@gmail.com>
2022-07-24 19:57:10 +0530

    3rd committ

--- app.js
+++ app.js
@@ -18,10 +18,28 @@ hbs.registerPartials(partials_path)
 // console.log(path.join(__dirname,"/public/index"))
 // console.log(path.join(__dirname,"../template/partials"))
 app.get("/",(req,res)=>{
-    res.render("register")
+    res.render("index")
 })
 app.get("/register",(req,res)=>{
     res.render("register")
+})
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+app.post("/login",async(req,res)=>{
+    try{
+        const email = req.body.email;
+        const password = req.body.password
+        const useremail = await Register.findOne({email:email})
+        if(useremail.password === password){
+            res.status(201).render("index");
+        }
+        else{
+            res.send("password are not matching")
+        }
+    }catch(error){
+        res.status(400).send("invalid login details")
+    }
 })
 app.post("/register",async(req,res)=>{
     try{
